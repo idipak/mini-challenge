fc61090de4e9624df48a939cdf13e5ca693f4621
idipak <dipak_pd@outlook.com>
2021-04-16 13:50:39 +0530

    My Solution

--- lib/themes/my_theme.dart
+++ lib/themes/my_theme.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme with ChangeNotifier {
   var _themeType = ThemeType.Light;
@@ -20,6 +22,7 @@ class MyTheme with ChangeNotifier {
       }
     };
   }
+
   void setThemeType(ThemeType themeType) {
     _themeType = themeType;
     notifyListeners();
@@ -29,11 +32,50 @@ class MyTheme with ChangeNotifier {
   ThemeData get currentThemeData {
     switch (themeType) {
       case ThemeType.Light:
-        return ThemeData();
+        return ThemeData(
+          appBarTheme: AppBarTheme(
+            color: Colors.deepOrangeAccent,
+            textTheme: TextTheme(
+                headline6: GoogleFonts.dancingScript().copyWith(fontSize: 25)),
+          ),
+          brightness: Brightness.light,
+          textTheme: TextTheme(
+            headline3: GoogleFonts.dancingScript(),
+            subtitle1: GoogleFonts.lato(),
+            bodyText1: GoogleFonts.lato(),
+          ),
+        );
       case ThemeType.Dark:
-        return ThemeData();
+        return ThemeData(
+          brightness: Brightness.dark,
+          appBarTheme: AppBarTheme(
+            textTheme: TextTheme(
+                headline6: GoogleFonts.dancingScript().copyWith(fontSize: 25)),
+          ),
+          textTheme: TextTheme(
+            headline3: GoogleFonts.dancingScript(),
+            subtitle1: GoogleFonts.lato(),
+            bodyText1: GoogleFonts.lato(),
+          ),
+        );
       case ThemeType.Other:
-        return ThemeData();
+        return ThemeData(
+          primaryColor: Colors.green,
+          canvasColor: Colors.lightGreenAccent,
+          accentColor: Colors.white,
+          appBarTheme: AppBarTheme(
+            textTheme: TextTheme(
+                headline6: GoogleFonts.dancingScript().copyWith(
+              fontSize: 25,
+            )),
+          ),
+          brightness: Brightness.light,
+          textTheme: TextTheme(
+            headline3: GoogleFonts.dancingScript(),
+            subtitle1: GoogleFonts.lato(),
+            bodyText1: GoogleFonts.lato(),
+          ),
+        );
     }
   }
 }
